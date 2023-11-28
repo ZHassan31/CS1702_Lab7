8428dccac613b86acff5aeded4d45b33547cb06c
Zubayr Hassan (Student) <2303419@brunel.ac.uk>
2023-11-28 15:15:04 +0000

    Update CS1702_Lab7.java
    Finished worksheet. must do Jar exercise on a new project then move on to the next worksheet

--- src/CS1702_Lab7.java
+++ src/CS1702_Lab7.java
@@ -1,13 +1,22 @@
 import java.text.DecimalFormat;
 import java.lang.Math;
+import java.io.File;
+import java.util.Random;
+import java.io.*;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class CS1702_Lab7 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException
 	{
 		//Q_Strings();
 		//Q_Mathematical_Functions();
-		
+		//Q_System_Commands();
+		//Q_Random_Numbers();
+		Q_File_Handling();
 	}
 		public static void Q_Strings()
 		{
@@ -43,5 +52,84 @@ public class CS1702_Lab7
 		System.out.println(s);
 
 		}
+		
+		public static void Q_System_Commands()
+		{
+			String dir_name = "C:\\ "; //Or another directory
+			File dir = new File(dir_name);
+				
+			File[] dir_list = dir.listFiles();
+			for(int i=0;i<dir_list.length;++i)
+			{
+				System.out.println(dir_list[i].getName());
+			}
 
+		}
+		
+		public static void Q_Random_Numbers() 
+		{
+			Random rand = new Random();
+			{
+				for(int i=0;i<1;++i)
+			{
+					System.out.println("1)" + rand.nextInt() % 101);
+			}
+			}
+			
+			{
+				for(int i=0;i<1;++i)
+			{
+					int x=rand.nextInt() % 10;
+					System.out.println("2)" + x+5);
+			}
+			}
+			
+			{
+				for(double i=0;i<1;++i)
+			{
+					System.out.println("3)" + rand.nextDouble());
+			}
+			}
+			
+			{
+				for(int i=0;i<1;++i)
+			{
+
+					double x=rand.nextDouble() % 1;
+					System.out.println("4)" + x);
+			}
+			}
+			
+
+		}
+		
+		public static void Q_File_Handling() throws IOException
+		{
+		//Write file	
+			{
+				   FileWriter writehandle = new FileWriter("\\\\ikb\\home\\19\\2303419\\Desktop\\Hello_World.txt");
+
+				   BufferedWriter bw = new BufferedWriter(writehandle);
+				   for(int i=0;i<10;++i)
+				   {
+				      String line = "My number is " + i;
+				      bw.write(line);
+				      bw.newLine();
+				   }
+				   bw.close();
+				   writehandle.close();
+				}
+			//Read file
+			{
+				   FileReader readhandle = new FileReader("\\\\ikb\\home\\19\\2303419\\Desktop\\Hello_World.txt");
+				   BufferedReader br = new BufferedReader(readhandle);
+				   String line = null;
+				   while((line = br.readLine()) != null)
+				   {
+				      System.out.println(line);
+				   }
+				   br.close();
+				   readhandle.close();
+				}
+		}
 }
